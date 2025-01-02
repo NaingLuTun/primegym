9bd2834d460fd9871cea1cb8e8ee71b8de432ed6
Naing Lu Tun <nainglu7924@gmail.com>
2025-01-02 10:57:02 +0630

    added seperate folder and file for pages, (added home page)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import Header from "./components/header/Header"
-import About from "./components/about/About"
-import Program from "./components/program/Program"
-import Schedule from "./components/schedule/Schedule"
-import Stats from "./components/stats/Stats"
-import Team from "./components/team/Team"
-import Testimonial from "./components/testimonial/Testimonial"
-import Blog from "./components/blog/Blog"
-import Footer from "./components/footer/Footer"
+import HomePage from "./pages/HomePage"
 function App() {
 
   return (
     <>
-      <Header />
-      <About />
-      <Program />
-      <Schedule />
-      <Stats />
-      <Team />
-      <Testimonial />
-      <Blog />
-      <Footer/>
+      <HomePage />
     </>
   )
 }
